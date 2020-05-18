877a0572e213b6e0fb8793b381c6ab46912e576c
vishalgoyal71 <vishalgoyal71@gmail.com>
2020-05-18 17:19:16 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,78 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  Students:any[];
+  constructor(){
+    this.Students=[
+    {
+      studentid:1,
+      name:'priya',
+      gender:'Female',
+      age:25,
+      course:'MCA'
+    },
+    {
+      studentid:2,
+      name:'monaa',
+      gender:'Female',
+      age:20,
+      course:'BCA'
+    },
+    {
+      studentid:3,
+      name:'amar',
+      gender:'Male',
+      age:28,
+      course:'MCA'
+    },
+    {
+      studentid:4,
+      name:'vishal',
+      gender:'Male',
+      age:28,
+      course:'BCA'
+    }
+    ];
+  }
+getmorestudents():void{
+this.Students=[
+    {
+      studentid:1,
+      name:'priya',
+      gender:'Female',
+      age:25,
+      course:'MCA'
+    },
+    {
+      studentid:2,
+      name:'monaa',
+      gender:'Female',
+      age:20,
+      course:'BCA'
+    },
+    {
+      studentid:3,
+      name:'amar',
+      gender:'Male',
+      age:28,
+      course:'MCA'
+    },
+    {
+      studentid:4,
+      name:'vishal',
+      gender:'Male',
+      age:28,
+      course:'BCA'
+    },
+     {
+      studentid:5,
+      name:'priti',
+      gender:'Female',
+      age:22,
+      course:'BCA'
+    }
+    ];
+
+  
+}
 }
